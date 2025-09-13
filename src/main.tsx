d10a867a52a7d974235792043b564f943a0044ce
thirdmadman <thirdmadman@gmail.com>
2025-09-13 10:57:36 +0200

    feat: update GTM usage

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App.tsx';
 
+import TagManager from 'react-gtm-module';
+
+if (!import.meta.env.DEV) {
+  const tagManagerArgs = {
+    gtmId: 'GTM-T7999C3W',
+  };
+
+  TagManager.initialize(tagManagerArgs);
+}
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
